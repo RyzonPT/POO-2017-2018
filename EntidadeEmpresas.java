6ea442f2a2fdccb2f8c9697653dc373972cd9103
Miguel <miguel_ratuinhu@hotmail.com>
2018-05-02 18:02:41 +0100

    Alterações insignificantes (LI3 Parody)

--- EntidadeEmpresas.java
+++ EntidadeEmpresas.java
@@ -1,4 +1,4 @@
-import java.time.LocalDate;
+
 public class EntidadeEmpresas extends FichaCliente
 {
     /**
@@ -10,98 +10,38 @@ public class EntidadeEmpresas extends FichaCliente
      * natureza da despesa, isto é, a actividade económica a que diz respeito;
      * valor da despesa.
     */
-   private String designacao;
-   private LocalDate data;
-   private int numeroFiscalCliente;
-   private String descricaoDaDespesa;
    private String actividadeEconomica;
-   private int valor;
-   private int deducao;
+   private int deducaoFiscal;
    
    public EntidadeEmpresas(){
-       this.designacao = "";
-       this.data = LocalDate.now();
-       this.numeroFiscalCliente = 0;
-       this.descricaoDaDespesa = "";
        this.actividadeEconomica = "";
-       this.valor = 0;
-       this.deducao = 0;
+       this.deducaoFiscal = 0;
     }
     
-    public EntidadeEmpresas(String designacao, LocalDate data, int numeroFiscalCliente, String descricaoDaDespesa, String actividadeEconomica, int valor, int deducao){
-       this.designacao = designacao;
-       this.data = data;
-       this.numeroFiscalCliente = numeroFiscalCliente;
-       this.descricaoDaDespesa = descricaoDaDespesa;
+    public EntidadeEmpresas(String actividadeEconomica, int deducaoFiscal){
        this.actividadeEconomica = actividadeEconomica;
-       this.valor = valor;
-       this.deducao = deducao;
+       this.deducaoFiscal = deducaoFiscal;
     }
     
     public EntidadeEmpresas(EntidadeEmpresas c){
-        this.designacao = c.getDesignacao();
-        this.data = c.getData();
-        this.numeroFiscalCliente = getNumeroFiscalCliente();
-        this.descricaoDaDespesa = getDescricaoDaDespesa();
         this.actividadeEconomica = getActividadeEconomica();
-        this.valor = getValor();
-        this.deducao = getDeducao();
-    }
-    
-    public String getDesignacao(){
-        return this.designacao;
-    }
-    
-    public LocalDate getData(){
-        return this.data;
-    }
-    
-    public int getNumeroFiscalCliente(){
-        return this.numeroFiscalCliente;
-    }
-    
-    public String getDescricaoDaDespesa(){
-        return this.descricaoDaDespesa;
+        this.deducaoFiscal = getDeducaoFiscal();
     }
     
     public String getActividadeEconomica(){
         return this.actividadeEconomica;
     }
     
-    public int getValor(){
-        return this.valor;
+    public int getDeducaoFiscal(){
+        return this.deducaoFiscal;
     }
-    
-    public int getDeducao(){
-        return this.deducao;
-    }
-    
-    public void setDesignacao(String designacao){
-        this.designacao = designacao;
-    }
-    
-    public void setData(LocalDate data){
-        this.data = data;
-    }
-    
-    public void setNumeroFiscalCliente(int numeroFiscalCliente){
-        this.numeroFiscalCliente = numeroFiscalCliente;
-    }
-    
-    public void setDescricaoDaDespesa(String descricaoDaDespesa){
-        this.descricaoDaDespesa = descricaoDaDespesa;
-    }
-    
+
     public void setActividadeEconomica(String actividadeEconomica){
         this.actividadeEconomica = actividadeEconomica;
     }
-    
-    public void setValor(int valor){
-        this.valor = valor;
-    }
-    
-    public void setDeducao(int deducao){
-        this.deducao = deducao;
+
+    public void setdeducaoFiscal(int deducaoFiscal){
+        this.deducaoFiscal = deducaoFiscal;
     }
     
     public EntidadeEmpresas clone(){
@@ -112,12 +52,7 @@ public class EntidadeEmpresas extends FichaCliente
         if(obj==this) return true;
         if(obj==null || obj.getClass() != this.getClass()) return false;
         EntidadeEmpresas le = (EntidadeEmpresas) obj;
-        return designacao.equals(le.getDesignacao()) &&
-               data.equals(le.getData()) &&
-               le.getNumeroFiscalCliente()==this.numeroFiscalCliente &&
-               descricaoDaDespesa.equals(le.getDescricaoDaDespesa()) &&
-               actividadeEconomica.equals(le.getActividadeEconomica()) &&
-               le.getValor()==this.valor &&
-               le.getDeducao()==this.deducao;
+        return le.getActividadeEconomica()==this.actividadeEconomica &&
+               le.getDeducaoFiscal()==this.deducaoFiscal;
     }
 }
