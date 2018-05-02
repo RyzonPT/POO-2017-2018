2e5fdbe162978dbe007b8970237105655fd0c502
RyzonPT <Filipe-Cunha1@hotmail.com>
2018-05-02 17:39:04 +0100

    fd

---
+++ Fatura.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+public class Fatura
+{
+   private String designacao;
+   private LocalDate data;
+   private int numeroFiscalCliente;
+   private String descricaoDaDespesa;
+   private String actividadeEconomica;
+}
