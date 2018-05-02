6fd2771ae9cf7b8796a61130135b0c64278f321c
Miguel <miguel_ratuinhu@hotmail.com>
2018-05-02 18:46:43 +0100

    Update Insignificante (LI3 Parody)Part2

---
+++ GestaoFichas.java
@@ -0,0 +1,34 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+public class GestaoFichas
+{
+    private Map<Integer,FichaCliente> fichas;
+    
+    public GestaoFichas()
+    {
+        this.fichas = new HashMap<>();
+    }
+    
+    public GestaoFichas(Map<Integer,FichaCliente> fch) {
+       this.fichas = fch.values().stream().collect(Collectors.toMap((e) -> e.getNumeroFiscal(),(e) -> e.clone()));
+    }
+    
+    public GestaoFichas(GestaoFichas a){
+        this.fichas=a.getfichas();
+    }
+    
+    public Map<Integer,FichaCliente> getfichas(){
+        return this.fichas.values().stream().collect(Collectors.toMap((e)->e.getNumeroFiscal(),(e)->e.clone()));
+    }
+    
+    public void addFicha ( FichaCliente a){
+        this.fichas.put(a.getNumeroFiscal(),a.clone());
+    }
+}
